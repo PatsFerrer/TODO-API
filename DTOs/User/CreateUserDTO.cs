456bb108f08f5fdc4cdfa213144fd918d1455540
Patricia Ferrer <patricia.ferreira@esx.com.br>
2025-04-23 11:40:55 -0300

    feat: DTOs de User

---
+++ DTOs/User/CreateUserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace TodoListApi.DTOs.User
+{
+    public class CreateUserDTO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ DTOs/User/UserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace TodoListApi.DTOs.User
+{
+    public class UserDTO
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+    }
+}
