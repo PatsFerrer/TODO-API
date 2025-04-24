6cfe337dd7f7ff480b85d481809434c02dc63bdd
Patricia Ferrer <patricia.ferreira@esx.com.br>
2025-04-24 10:20:16 -0300

    refact: altera senha

--- Models/User.cs
+++ Models/User.cs
@@ -4,6 +4,6 @@
     {
         public Guid Id { get; set; }
         public string Username { get; set; }
-        public string Password { get; set; }
+        public string PasswordHash { get; set; }
     }
 }
