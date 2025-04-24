b1f7bdb25c15f7f37158d0144eababbd813c0bc6
Patricia Ferrer <patricia.ferreira@esx.com.br>
2025-04-24 10:18:34 -0300

    feat: implementa controller de user

---
+++ Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoListApi.DTOs.User;
+using TodoListApi.Services;
+using TodoListApi.Services.Interface;
+
+namespace TodoListApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _service;
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(IUserService service, ILogger<UserController> logger)
+        {
+            _service = service;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateUserDTO dto)
+        {
+            var user = await _service.CreateUserAsync(dto);
+            _logger.LogInformation("User created successfully with username: {Username}", dto.Username);
+            return Ok(user);
+        }
+    }
+}
