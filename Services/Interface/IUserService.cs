604652db0b299a833e78d7f97bb29d93997e3c6d
Patricia Ferrer <patricia.ferreira@esx.com.br>
2025-04-24 10:13:52 -0300

    feat: interface do service de user

---
+++ Services/Interface/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using TodoListApi.DTOs.User;
+
+namespace TodoListApi.Services.Interface
+{
+    public interface IUserService
+    {
+        Task<UserDTO> CreateUserAsync(CreateUserDTO dto);
+        //Task<UserDTO> GetUserByIdAsync(Guid id);
+        //Task<IEnumerable<UserDTO>> GetAllUsers();
+    }
+}
