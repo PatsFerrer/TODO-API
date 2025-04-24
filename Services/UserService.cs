ad530e10aa64cd644d995d9077c8a4d43fa440fc
Patricia Ferrer <patricia.ferreira@esx.com.br>
2025-04-24 10:14:38 -0300

    feat: implementa service de user

---
+++ Services/UserService.cs
@@ -0,0 +1,35 @@
+﻿using TodoListApi.DTOs.User;
+using TodoListApi.Models;
+using TodoListApi.Repositories.Interface;
+using TodoListApi.Services.Interface;
+
+namespace TodoListApi.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _repository;
+
+        public UserService(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<UserDTO> CreateUserAsync(CreateUserDTO dto)
+        {
+            var user = new User
+            {
+                Id = Guid.NewGuid(),
+                Username = dto.Username,
+                PasswordHash = dto.Password
+            };
+
+            var createdUser = await _repository.CreateAsync(user);
+
+            return new UserDTO
+            {
+                Id = createdUser.Id,
+                Username = createdUser.Username
+            };
+        }
+    }
+}
