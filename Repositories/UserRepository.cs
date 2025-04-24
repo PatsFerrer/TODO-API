74804dd2706805ccfe07d2c6c89a8a7123013f5e
Patricia Ferrer <patricia.ferreira@esx.com.br>
2025-04-24 10:17:20 -0300

    feat: implementa repositorio de user

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using TodoListApi.Models;
+using TodoListApi.Repositories.Interface;
+
+namespace TodoListApi.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly string _connectionString;
+
+        public UserRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<User> CreateAsync(User user)
+        {
+            var query = File.ReadAllText("Data/Users/CreateUser.sql");
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.ExecuteAsync(query, user);
+            }
+
+            return user;
+
+        }
+    }
+}
