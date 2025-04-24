21ac8e67bf7a6a88322ed427f6961c40690813c7
Patricia Ferrer <patricia.ferreira@esx.com.br>
2025-04-24 10:16:59 -0300

    feat: interface do repositorio de user

---
+++ Repositories/Interface/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using TodoListApi.Models;
+
+namespace TodoListApi.Repositories.Interface
+{
+    public interface IUserRepository
+    {
+        Task<User> CreateAsync(User user);
+        //Task<IEnumerable<User>> GetAllAsync();
+    }
+}
