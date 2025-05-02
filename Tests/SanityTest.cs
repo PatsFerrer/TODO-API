9fe54c3cd2519fea2e8ea4acad4aa31ac417100e
Patricia Ferrer <patricia.ferreira@esx.com.br>
2025-05-02 10:07:33 -0300

    test: Adiciona teste de exemplo

---
+++ Tests/SanityTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace TodoListApi.Tests
+{
+    public class SanityTest
+    {
+        [Fact]
+        public void Should_Run_Test_Pipeline()
+        {
+            Assert.True(true);
+        }
+    }
+}
